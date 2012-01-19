256f21d3c914e4ebe257af3074ba088c57f11cc4
Michael Thompson <thompson.mi@husky.neu.edu>
2012-01-19 00:06:08 -0500

    Implemented all 7 modifications to 02_logs/stats.rb (ignore header of log, various counting outputs)

--- 01_ruby/02_logs/stats.rb
+++ 01_ruby/02_logs/stats.rb
@@ -7,24 +7,54 @@ unless filename                         # we can't work without a filename
 end
 
 lines = 0                               # a humble line counter
-unique_users = "unknown"                # someday, this will work
-unique_pages = "unknown"                # someday, this will work
-most_active_day = "unknown"             # someday, this will work
-most_active_user = "unknown"            # someday, this will work
-most_active_page = "unknown"            # someday, this will work
+# hash naming convention?
+user_activity_hash = {}                 # username : # of pageloads
+url_hitcount_hash = {}                  # url : total # of hits
+date_activity_hash = {}                 # date : # of hits on that date
+url_uniques_hash = {}                   # url : {user : user}
 
 open(filename).each do |m|              # loop over every line of the file
+  lines += 1                            # bump the counter
+  next if lines == 1                    # ignore the header line, skip to the next line
   m.chomp!                              # remove the trailing newline
   values = m.split(",")                 # split comma-separated fields into a values array
-
-  # ...
-
-  lines += 1                            # bump the counter
+  date, userID, url = values[0], values[1], values[2]
+  
+  # cleaner with ternary operators?  
+  if user_activity_hash[userID].nil? # update the hash of {user : pagecount}
+    user_activity_hash[userID] = 0
+  else
+    user_activity_hash[userID] += 1
+  end
+  
+  if url_hitcount_hash[url].nil?      # update the hash of {url : hitcount}
+    url_hitcount_hash[url] = 0
+  else
+    url_hitcount_hash[url] += 1
+  end
+  
+  if date_activity_hash[date].nil?   # update the hash of {date : activity}
+    date_activity_hash[date] = 0
+  else
+    date_activity_hash[date] += 1
+  end
+  
+  if url_uniques_hash[url].nil?       # update the hash of {url : {user : hitcount}}
+    url_uniques_hash[url] = { userID => 1 }
+  elsif (url_uniques_hash[url][userID].nil?)
+    url_uniques_hash[url][userID] = 1
+  else
+    url_uniques_hash[url][userID] += 1
+  end
 end
 
-puts "total lines: #{lines}"                  # output stats
-puts "unique users: #{unique_users}"          # someday, this will work
-puts "unique pages: #{unique_pages}"          # someday, this will work
-puts "most active day: #{most_active_day}"    # someday, this will work
-puts "most active user: #{most_active_user}"  # someday, this will work
-puts "most active page: #{most_active_page}"  # someday, this will work
+puts "total lines: #{lines - 1}"            # output stats (excludes the header line)
+puts "unique users: #{user_activity_hash.length}" 
+puts "unique pages: #{url_hitcount_hash.length}"
+# sort the hashes, select the last item (because we want "most"), retrieve the key(at [0])
+unless lines <= 1                           # prevent crash when log file is empty
+  puts "most active day: #{date_activity_hash.sort_by { |date, activity| activity }.last[0]}" 
+  puts "most active user: #{user_activity_hash.sort_by { |user, activity| activity }.last[0]}" 
+  puts "most active page: #{url_hitcount_hash.sort_by { |url, hitcount| hitcount }.last[0]}" 
+  puts "most popular page (by unique users): #{url_uniques_hash.sort_by { |url, userlist| userlist.length }.last[0]}"
+end
