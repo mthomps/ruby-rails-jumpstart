72e95bb1096948dc4d69572623014a24f623113f
Michael Thompson <thompson.mi@husky.neu.edu>
2012-02-27 13:55:57 -0500

    Just about finished with 02restful challenging tasks. TODO: Catch exception when gender/name validation fails

--- 02_web/02_restful/application.rb
+++ 02_web/02_restful/application.rb
@@ -5,6 +5,9 @@ require 'sinatra'
 require 'sinatra/base'
 require 'supermodel'
 require 'json'
+require 'open-uri'
+require 'people_places_things'
+include PeoplePlacesThings
 
 #
 # For documentation, see:
@@ -19,13 +22,87 @@ end
 
 # find_by_attribute(inventorID)
 class Inventor < SuperModel::Base
-  attributes :name
+  validate do |inventor|
+    puts "SHIT!"
+    json_result = inventor.gender_detect
+    status = json_result["status"]
+    gender_result = json_result["answer"]
+    puts gender_result
+    probability = json_result["likelihood"]
+    if inventor.gender.nil? or (status.casecmp("NOT FOUND") == 0)
+      puts "no gender or rapleaf gender detection failed"
+      return
+    elsif ((gender_result["gender"].casecmp(inventor.gender) == 0) && probability.to_f >= 0.8) 
+      puts "gender matches, probability is >80%"
+      return
+    else
+      # ActiveRecord::RecordInvalid?
+      # Adding an error here, how to rescue it later?
+      puts "gender does NOT match name!! raise and error!"
+      raise InvalidRecord, "blah", caller
+      inventor.errors[:gender] << "Invalid record: gender should match name"
+    end
+  end
+  validate :gender_matches_name, :on => :save
+  attributes :name, :gender
   validates :name, :presence => true, :uniqueness => true
-end
-
+  validates :gender, :presence => true
   
+  def first_name
+    person_name = PersonName.new(self.name)
+    if person_name.first.nil?
+      self.name
+    else
+      person_name.first
+    end
+  end
+  
+  def last_name
+    person_name = PersonName.new(self.name)
+    person_name.last
+  end
+  
+  def gender_matches_name
+
+  end
+  
+  # given a name, returns rapleaf gender guess JSON object
+  def gender_detect
+    base_url = "https://www.rapleaf.com/developers/try_name_to_gender?query="
+    query_url = base_url + URI.escape(self.first_name)
+    object = open(query_url) do |v|
+      input = v.read
+      JSON.parse(input)
+    end
+    object
+  end
+end
+=begin
+class genderValidator < ActiveModel::Validator
+  def validate(inventor)
+    puts "SHIT!"
+    json_result = self.gender_detect
+    status = json_result["status"]
+    gender_result = json_result["answer"]
+    probability = json_result["likelihood"]
+    if self.gender.nil? or (status.casecmp("NOT FOUND") == 0)
+      puts "no gender or rapleaf gender detection failed"
+      return
+    elsif ((gender_result["gender"].casecmp(self.gender) == 0) && probability.to_f >= 0.8) 
+      puts "gender matches, probability is >80%"
+      return
+    else
+      # ActiveRecord::RecordInvalid?
+      # Adding an error here, how to rescue it later?
+      puts "gender does NOT match name!! raise and error!"
+      raise InvalidRecord, "blah", caller
+      record.errors[:gender] << "Invalid record: gender should match name"
+    end
+  end  
+end    
+=end
 class RestfulServer < Sinatra::Base
-  ANON = Inventor.create!(:name => "ANONYMOUS")
+  ANON = Inventor.create!(:name => "ANONYMOUS", :gender => "male")
   # helper method that returns json
   def json_out(data)
     content_type 'application/json', :charset => 'utf-8'
@@ -61,7 +138,7 @@ class RestfulServer < Sinatra::Base
   def list_inventors
     json_out(Inventor.all)
   end
-
+  
   # display the list of inventors
   get '/inventors' do
     list_inventors
@@ -95,8 +172,9 @@ class RestfulServer < Sinatra::Base
         inv = Inventor.new(givenInv)
         begin
           inv.save!
-        rescue
+        rescue Exception => e
           puts "Failed to save inventor, no name"
+          puts e.message
           bad_request
         end
       end
@@ -108,8 +186,9 @@ class RestfulServer < Sinatra::Base
       idea.inventor = inv
     begin
       idea.save!
-    rescue
+    rescue Exception => e
       puts "Failed to save idea, no category/text"
+      puts e.message
       bad_request
     end
 
@@ -136,7 +215,8 @@ class RestfulServer < Sinatra::Base
     idea = Idea.find(params[:id])
     begin
       idea.update_attributes!(JSON.parse(request.body.read))
-    rescue
+    rescue Exception => e
+      puts e.message
       puts "Failed to update idea, no category/text"
       bad_request
     end
